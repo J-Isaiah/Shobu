1c22579f0a092eb2df5527ced8ead798241802af
Isaiah Jacobsen <isaiah.jacobsen1@gmail.com>
2026-06-22 20:28:56 -0500

    Add Docker deployment setup

--- backend/src/main/java/com/shobu/api/WebSocketConfig.java
+++ backend/src/main/java/com/shobu/api/WebSocketConfig.java
@@ -12,8 +12,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173");
-
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins(
+                        "http://localhost:5173",
+                        "http://localhost:3000"
+                );
     }
 
     @Override
