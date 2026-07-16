ea4e51fcbb6f4cd3050ac541d7ec0ae839c6b7d7
Isaiah Jacobsen <isaiah.jacobsen1@gmail.com>
2026-07-15 20:27:08 -0500

    fix env error

--- backend/src/test/java/com/shobu/ShobuApplicationTests.java
+++ backend/src/test/java/com/shobu/ShobuApplicationTests.java
@@ -3,11 +3,13 @@ package com.shobu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"app.players.isaiah-id=00000000-0000-0000-0000-000000000001",
+		"app.players.julia-id=00000000-0000-0000-0000-000000000002"
+})
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
